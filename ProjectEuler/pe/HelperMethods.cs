d12058a4a402cdac086142d734078f7c214ac788
Stuart Stein <qmstuart@gmail.com>
2016-09-28 14:25:41 +0100

    Solved Problems 9, 10 and 11. Rewrote prime number sieve to be much faster.

--- ProjectEuler/pe/HelperMethods.cs
+++ ProjectEuler/pe/HelperMethods.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ProjectEuler
 {
 	public static class HelperMethods
@@ -5,45 +8,35 @@ namespace ProjectEuler
 		public static int GetFactorial(int n)
 		{
 			var f = 1;
-
 			for (var x = 1; x <= n; x++)
-			{
 				f *= x;
-			}
-
 			return f;
 		}
-
 		public static int GetNextFibonnaci(int x, int y)
 		{
 			return x + y;
 		}
-
 		public static int FindHighestFactor(uint x, int lowerBound, int upperBound)
 		{
 			var factor = upperBound;
-
 			while(factor > lowerBound)
 			{
 				if ((x % factor == 0) && (x / factor <= upperBound))
 					return factor;
-
 				factor--;
 			}
-
 			return 0;
 		}
-
 		public static string Reverse(string toReverse)
 		{
 			var reversed = string.Empty;
-
 			for(int x = toReverse.Length - 1; x >= 0; x--)
-			{
 				reversed += toReverse[x];
-			}
-
 			return reversed;
+		}
+		public static ulong Sum(this IEnumerable<ulong> source)
+		{
+			return source.Aggregate((x, y) => x + y);
 		}
 	}
 }
--- ProjectEuler/pe/PrimeNumberSieve.cs
+++ ProjectEuler/pe/PrimeNumberSieve.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace ProjectEuler
 {
@@ -10,47 +9,34 @@ namespace ProjectEuler
 		public IList<ulong> Sieve(ulong upperBound)
 		{
 			var primes = GetRangeInclusive(upperBound);
-
-			for(var x = 0; x < primes.Count; x++)
+			for(var x = 0; x <= Math.Sqrt(primes.Count); x++)
 			{
-				var y = primes[x];
-
-				if(y == 0) continue;
-
-				var locationStep = (ulong)x;
-
-				if (IsPrime(y))
+				if(primes[x] != 0)
 				{
-					do
+					var i = primes[x];
+					var iSquared = i*i;
+					var c = (ulong)0;
+					ulong indexToZero;
+					while ((indexToZero = iSquared + c * i) < (ulong)primes.Count+2)
 					{
-						locationStep += y;
-						if(locationStep < (ulong)primes.Count)
-							primes[(int)locationStep] = 0;
-
-					} while (locationStep < (ulong)primes.Count);
+						primes[(int)indexToZero - 2] = 0;
+						c++;
+					}
 				}
 			}
 			return primes.Select(x => x).Where(x => x != 0).ToList();
 		}
-
 		public bool IsPrime(ulong x)
 		{
 			for(ulong c = 2; c <= Math.Sqrt(x); c++)
-			{
-				if(x % c == 0)
-					return false;
-			}
-
+				if(x % c == 0) return false;
 			return true;
 		}
-
 		private IList<ulong> GetRangeInclusive(ulong upperBound)
 		{
 			var range = new List<ulong>();
-			
 			for (ulong x = 2; x <= upperBound; x++)
 				range.Add(x);
-
 			return range;
 		}
 	}
